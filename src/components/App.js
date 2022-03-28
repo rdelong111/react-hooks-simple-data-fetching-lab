559bf74d9d539666fa5ea11898a7cdd11cf79015
Ryan DeLong <rdelong111@gmail.com>
2022-03-27 19:30:20 -0500

    commit tree ce51e6fe4a2cf8fbab78bf2fcf118ef2c6a99a87

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,26 @@
-// create your App component here
+import React, {useState, useEffect} from "react";
+
+function App() {
+  const [theDog, changeDog] = useState({
+    message: '',
+    status: ''
+  });
+  const [isLoaded, setLoaded] = useState(false);
+
+  useEffect(() => {
+    fetch('https://dog.ceo/api/breeds/image/random')
+      .then(r => r.json())
+      .then(dog => {
+        changeDog(dog);
+        setLoaded(true);
+      });
+  }, []);
+  
+  if (!isLoaded) return <p>Loading...</p>;
+
+  return (
+    <img src={theDog.message} alt="A Random Dog" />
+  )
+}
+
+export default App;
